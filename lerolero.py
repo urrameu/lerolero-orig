a46a56226109f8793061d81736fa2a497907cdd4
Alan Turing <turing@machine.com>
2020-11-18 14:02:38 -0300

    Insere os trechos das frases

--- lerolero.py
+++ lerolero.py
@@ -8,9 +8,23 @@ import random
 # Cada frase é composta por três partes aleatórias; aqui,
 # listas de possibilidades para cada uma das partes.
 
-parte1 = []
-parte2 = []
-parte3 = []
+parte1 = [
+    "O sistema em desenvolvimento",
+    "O novo protocolo de comunicação",
+    "O algoritmo otimizado"
+    ]
+
+parte2 = [
+    "possui excelente desempenho",
+    "oferece garantias de precisão acima da média",
+    "preenche uma lacuna significativa"
+    ]
+
+parte3 = [
+    "nas aplicações a que se destina",
+    "em relação às opções disponíveis no mercado",
+    "e provê ampla vantagem competitiva a seus usuários"
+    ]
 
 # Combina as partes aleatoriamente
 print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
