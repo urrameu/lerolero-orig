45caee3b26815b513b0098878fea500e45793c30
Alan Turing <turing@machine.com>
2020-11-18 12:05:19 -0300

    Implementa esqueleto do programa

--- lerolero.py
+++ lerolero.py
@@ -2,3 +2,15 @@
 """Gerador de lero-lero.
 
 Gera frases de efeito sem significado real."""
+
+import random
+
+# Cada frase é composta por três partes aleatórias; aqui,
+# listas de possibilidades para cada uma das partes.
+
+parte1 = []
+parte2 = []
+parte3 = []
+
+# Combina as partes aleatoriamente
+print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
