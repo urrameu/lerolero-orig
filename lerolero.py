1aaa00495786bd75d33b4510747da0b2ebd413ac
Ada Lovelace <ada@analyticalengine.com>
2020-11-18 13:57:47 -0300

    Cria esqueleto da versão multi-línguas

--- lerolero.py
+++ lerolero.py
@@ -12,5 +12,12 @@ parte1 = []
 parte2 = []
 parte3 = []
 
+lingua = int(input("Escolha a língua: 1 - português; 2 - inglês\n "))
+
+if lingua == 2:
+    parte1 = []
+    parte2 = []
+    parte3 = []
+
 # Combina as partes aleatoriamente
 print (random.choice(parte1), random.choice(parte2), random.choice(parte3))
